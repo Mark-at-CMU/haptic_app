cb593e6a00b07b307b6a37c7a3bc1341df33978b
Mark Ma <mark@Marks-MacBook-Air.local>
2021-04-10 13:09:12 -0400

    first commit

---
+++ haptic_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  haptic_app
+//
+//  Created by Mark Ma on 4/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
